01f0cfab92edcc63892d97826b9afb910810d6f1
bacnn <bacnn@rever.vn>
2023-05-18 10:14:00 +0700

    update sth

--- src/test/java/model/IAuthentication.java
+++ src/test/java/model/IAuthentication.java
@@ -4,6 +4,6 @@ import org.apache.commons.codec.binary.Base64;
 
 public interface IAuthentication {
 
-    String email = "bacnn24@gmail.com";
-    String apiToken = "ATATT3xFfGF0gfwT3lgTfZ56rWW-Gr8Nop4M7gSpW_WBpqy9G9lg0fvd863qM2eSGivkSN5rFl5gQ4rj_zFhZl3sgSiVE3M2xxvZzwuG_FdyY7GPceNTNqMYGG2gY9YHRhOJymQuk-_JVYjA88fISERYTswON6jIK0l-jabUAYD34Ff6vI4WMMs=F62AF896";
+    String email = "email";
+    String apiToken = "token";
 }
